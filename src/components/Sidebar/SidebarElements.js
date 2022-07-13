0827d14a4a0f8e8d42d583a3ae071fe07a84b41a
albert <albertnt905@gmail.com>
2022-07-13 16:59:19 -0400

    -

---
+++ src/components/Sidebar/SidebarElements.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+export const SidebarContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 37px 10px 10px;
+    gap: 68px;
+    width: 563px;
+    height: 1574px;
+    background: #FFFFFF;
+    flex: none;
+    order: 0;
+    align-self: stretch;
+    flex-grow: 0;
+`
+
+export const SidebarTitleWrapper = styled.div`
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 35px 17px;
+    gap: 10px;
+    width: 512px;
+    height: 160px;
+    background: #FFFFFF;
+    border: 1px solid #000000;
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+`
+
+export const SidebarTitleTextWrapper = styled.div`
+    box-sizing: border-box;
+    width: 337px;
+    height: 92px;
+    background: #213212;
+    border: 1px solid #000000;
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+`
+
+export const SidebarTitleLogoWrapper = styled.div`
+    box-sizing: border-box;
+    width: 121px;
+    height: 92px;
+    background: #FFFFFF;
+    border: 1px solid #000000;
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+`
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SidebarContainer } from './SidebarElements'
+const Sidebar = () => {
+  return (
+    <SidebarContainer>index</SidebarContainer>
+  )
+}
+
+export default Sidebar
