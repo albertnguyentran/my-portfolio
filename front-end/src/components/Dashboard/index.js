c98e1b1cbcc1fdeb46140e530d7dc8706bc5b238
albert <albertnt905@gmail.com>
2022-08-18 03:31:00 -0400

    -

--- front-end/src/components/Dashboard/index.js
+++ front-end/src/components/Dashboard/index.js
@@ -1,14 +1,15 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import { DashboardContainer, StockContainer, StockWrapper, HeaderWrapper, GraphContainer, GraphWrapper } from './DashboardElements'
 import axios from 'axios'
 
 export default function Dashboard(props){
 
     const [user, setUser] = useState(props.user)
-    
-    async function handleSubmit(){
-        alert('urmom')
-    }
+    const [stocks, setStocks] = useState({ticker: '', amount: '', date: '', price: ''})
+
+    const handleChange = (e) => {
+        setStocks({...stocks, [e.target.name]: e.target.value})
+    };
 
     useEffect(() => {
         async function fetchData(){
@@ -23,10 +24,24 @@ export default function Dashboard(props){
         fetchData()
     }, []);
 
+    async function handleSubmit(){
+        try {
+            const response = await axios.post('http://localhost:5000/api/stocks', {
+                portfolioName: props.id,
+                ticker: stocks.ticker,
+                amonunt: stocks.amount,
+                date: stocks.date,
+                price: stocks.price
+            })
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     var arr = user.user.portfolios[0].stocks
     var renderedOutput = arr.map(item => <div className=""> {item.ticker} </div>)
     
-
     return (
         <DashboardContainer>
                 {renderedOutput}
@@ -34,14 +49,13 @@ export default function Dashboard(props){
             <StockContainer>
                 <StockWrapper>
                     <form onSubmit={handleSubmit}>
-                        <input></input>
-                        <input></input>
-                        <input></input>
-                        <input></input>
+                        <input type="text" name="ticker" onChange={handleChange} value={stocks.ticker}></input>
+                        <input type="text" name="amount" onChange={handleChange} value={stocks.amount}></input>
+                        <input type="text" name="price" onChange={handleChange} value={stocks.price}></input>
                         <input type="submit"></input>
                     </form>
                 </StockWrapper>
-                
+
                 <StockWrapper></StockWrapper>
 
             </StockContainer>
